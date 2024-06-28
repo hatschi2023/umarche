91a65ba3ca8c32622b750401d123153470c14101
hatschi2023 <145645279+hatschi2023@users.noreply.github.com>
2024-06-28 21:46:29 +0900

    フォームリクエスト

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,9 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
-
 use Intervention\Image\Laravel\Facades\Image;//imageアップロード
-
+use App\Http\Requests\UploadImageRequest;
 
 class ShopController extends Controller
 {
@@ -51,7 +50,7 @@ class ShopController extends Controller
         return view('owner.shops.edit', compact('shop'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
         $imageFile = $request->image; //一時保存
         if(!is_null($imageFile) && $imageFile->isValid()){
