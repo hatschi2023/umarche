29ecdda6a33bfb5184bb5b60b1812401ab2d2e64
hatschi2023 <145645279+hatschi2023@users.noreply.github.com>
2024-07-01 14:08:04 +0900

    product初期設定

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,13 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         $this->call([
             AdminSeeder::class,
             OwnerSeeder::class,
             ShopSeeder::class,
             ImageSeeder::class,
             CategorySeeder::class,
+            ProductSeeder::class,
+
         ]);
     }
 }
