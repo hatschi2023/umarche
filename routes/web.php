855de9527946cd893bfe82915abed0bbb67b0eed
hatschi2023 <145645279+hatschi2023@users.noreply.github.com>
2024-07-04 14:09:52 +0900

    user.items.調整

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,17 @@ use App\Http\Controllers\User\ItemController;
 |
 */
 
-Route::get('/', function () {
-    return view('user.welcome');
-});
+// Route::get('/', function () {
+//     return view('user.welcome');
+// });
 
 Route::middleware('auth:users')->group(function(){
-        Route::get('/', [ItemController::class, 'index'])->name('items.index');
+    Route::get('/', [ItemController::class, 'index'])->name('items.index');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('user.dashboard');
-// })->middleware(['auth:users'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('user.dashboard');
+})->middleware(['auth:users'])->name('dashboard');
 
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
