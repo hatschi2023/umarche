772845377ec57fd5ba5e521d8fe3b53127fb1a87
hatschi2023 <145645279+hatschi2023@users.noreply.github.com>
2024-07-04 14:45:44 +0900

    product factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Product;
 use Illuminate\Database\Seeder;
+use App\Models\Stock;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,9 +21,11 @@ class DatabaseSeeder extends Seeder
             ShopSeeder::class,
             ImageSeeder::class,
             CategorySeeder::class,
-            ProductSeeder::class,
-            StockSeeder::class,
+            // ProductSeeder::class,
+            // StockSeeder::class,
             UserSeeder::class,
         ]);
+        Product::factory(100)->create();
+        Stock::factory(100)->create();
     }
 }
