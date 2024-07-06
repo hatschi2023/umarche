0ba72fa27f3238779a2a94f16df1ffadf942bad2
hatschi2023 <145645279+hatschi2023@users.noreply.github.com>
2024-07-06 23:58:42 +0900

    cart ralation

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,11 @@ Route::middleware('auth:users')->group(function(){
     Route::get('/', [ItemController::class, 'index'])->name('items.index');
 });
 
+Route::prefix('cart')->middleware('auth:users')->group(function(){
+    Route::get('/', [CartController::class,'index'])->name('cart.index');
+    Route::post('add', [CartController::class, 'add'])->name('cart.add');
+});
+
 Route::get('show/{item}', [ItemController::class, 'show'])->name('items.show');
 
 Route::get('/dashboard', function () {
